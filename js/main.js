a3b2d43dcfc3aa973e912f1f15e97b01958188e2
ko <kyokucho1989@gmail.com>
2025-09-05 05:51:01 +0900

    ballクラスとRenderクラスを作成

--- js/main.js
+++ js/main.js
@@ -1,21 +1,17 @@
+import Ball from "./ball.js";
+import Renderer from "./renderer.js";
+
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 
-let x = canvas.width / 2;
-let y = canvas.height - 30;
+const renderer = new Renderer(ctx);
+const balls = [new Ball(canvas.width / 2, canvas.height - 30, 2, -2)];
 
-let dx = 2;
-let dy = -2;
-
-function draw() {
+function gameLoop() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.beginPath();
-  ctx.arc(x, y, 10, 0, Math.PI * 2);
-  ctx.fillStyle = "#0095DD";
-  ctx.fill();
-  ctx.closePath();
-  x += dx;
-  y += dy;
+
+  balls.forEach((ball) => ball.update());
+  renderer.render(balls);
 }
 
-setInterval(draw, 10);
+setInterval(gameLoop, 10);
